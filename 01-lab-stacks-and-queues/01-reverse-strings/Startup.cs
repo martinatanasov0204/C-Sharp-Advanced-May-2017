e384b6feb41270e886bcfc750c8f28c983d045dd
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-05-26 12:34:07 +0300

    Add files via upload

---
+++ 01-lab-stacks-and-queues/01-reverse-strings/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_reverse_strings
+{
+    using System;
+    using System.Collections.Generic;
+    class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            Stack<char> reversedInput = new Stack<char>();
+
+            foreach (char symbol in input)
+            {
+                reversedInput.Push(symbol);
+            }
+
+            Console.WriteLine(string.Join("", reversedInput));
+        }
+    }
+}
