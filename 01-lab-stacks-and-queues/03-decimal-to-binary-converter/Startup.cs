4964ba4b8554bd9f6d9b93eb9025c3f2225f80f0
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-05-26 12:35:37 +0300

    Add files via upload

---
+++ 01-lab-stacks-and-queues/03-decimal-to-binary-converter/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace _03_decimal_to_binary_converter
+{
+    using System;
+    using System.Collections.Generic;
+    class Startup
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Stack<int> binaryStack = new Stack<int>();
+
+            if (num == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                while (num > 0)
+                {
+                    var reminder = num%2;
+                    binaryStack.Push(reminder);
+
+                    num /= 2;
+                }
+            }
+
+            while (binaryStack.Count > 0)
+            {
+                Console.Write(binaryStack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
