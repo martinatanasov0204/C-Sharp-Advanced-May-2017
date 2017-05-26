0c0153735317466e6cd8769512a5f71fb127a449
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-05-26 12:35:08 +0300

    Add files via upload

---
+++ 01-lab-stacks-and-queues/02-simple-calculator/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace _02_simple_calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Stack<string> data = new Stack<string>();
+
+            foreach (var symbol in input)
+            {
+                data.Push(symbol);
+            }
+            // Reverse the stack
+
+            Stack<string> reversedData = new Stack<string>();
+            while (data.Count > 0)
+            {
+                reversedData.Push(data.Pop());
+            }
+
+            double result = double.Parse(reversedData.Pop());
+
+            while (reversedData.Count > 0)
+            {
+                if (reversedData.Peek() == "+")
+                {
+                    reversedData.Pop();
+                    result += double.Parse(reversedData.Pop());
+                }
+                else if (reversedData.Peek() == "-")
+                {
+                    reversedData.Pop();
+                    result -= double.Parse(reversedData.Pop());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
