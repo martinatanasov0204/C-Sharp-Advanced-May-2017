692f632d4e34e9d1f9581b98a60673832a5c7a82
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-05-26 12:36:00 +0300

    Add files via upload

---
+++ 01-lab-stacks-and-queues/04-matching-brackets/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace _04_matching_brackets
+{
+    using System;
+    using System.Collections.Generic;
+    class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startPos = indexes.Pop();
+                    int endPos = i;
+
+                    var toPrint = input.Substring(startPos, endPos - startPos + 1);
+
+                    Console.WriteLine(toPrint);
+                }
+            }
+        }
+    }
+}
